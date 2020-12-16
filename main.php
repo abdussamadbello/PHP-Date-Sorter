6ba7e9b3fcf390197390e796637adacb57512fc0
abdussamadbello <abdussamadbello4real@hotmail.co.uk>
2020-12-16 00:44:59 +0000

    Initial commit

---
+++ main.php
@@ -0,0 +1,76 @@
+<?php
+$json_data=
+    '[{
+        "mon": "Jan",
+        "month_orders": "349"
+    },
+    {
+        "mon": "Feb",
+        "month_orders": "414"
+    },
+    {
+        "mon": "Mar",
+        "month_orders": "401"
+    },
+    {
+        "mon": "Apr",
+        "month_orders": "481"
+    },
+    {
+        "mon": "May",
+        "month_orders": "444"
+    },
+    {
+        "mon": "Jun",
+        "month_orders": "526"
+    },
+    {
+        "mon": "Jul",
+        "month_orders": "565"
+    },
+    {
+        "mon": "Aug",
+        "month_orders": "591"
+    },
+    {
+        "mon": "Sep",
+        "month_orders": "800"
+    },
+    {
+        "mon": "Oct",
+        "month_orders": "848"
+    },
+    {
+        "mon": "Nov",
+        "month_orders": "557"
+    },
+        {
+        "mon": "Mar",
+        "month_orders": "401"
+    },
+    {
+        "mon": "Apr",
+        "month_orders": "481"
+    },
+    {
+        "mon": "May",
+        "month_orders": "444"
+    },
+    {
+        "mon": "Jun",
+        "month_orders": "526"
+    }]';
+
+    $data=json_decode($json_data,true);
+    $month  = array_column($data, 'mon');
+    $month_orders = array_column($data, 'month_orders');
+    $name='mon';
+    function date_compare($a, $b)
+     {
+    $t1 = strtotime($a['mon']);
+    $t2 = strtotime($b['mon']);
+    return $t1 - $t2;
+   }    
+   usort($data, 'date_compare');
+   print_r($data);
+    ?>
